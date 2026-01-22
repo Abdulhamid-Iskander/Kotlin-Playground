4925ddf66027e71350ee6d8d6f7106dc68822eea
caesar <abdulhamidiskander@gmail.com>
2026-01-22 17:46:19 +0200

    Data types

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -20,5 +20,28 @@ fun main(){
 //Compile-time constants must be declared at the top level of a file,
 // or inside an object or a companion object.
 // They cannot be declared inside a class body or a function.
+ // Data types
+//    Kotlin provides signed fixed-width integers: Byte (8-bit),
+//    Short (16-bit), Int (32-bit), and Long (64-bit).
+//    The default inference for non-decimal numbers is always Int.
+//    If the number exceeds Int range,
+//    you must denote it with the 'L' suffix to specify Long.
+    val age :Int = 21
+
+
+    val isOnline: Boolean = true
+    val isFinished: Boolean = false
+
+    val letter : Char = 'A'
+    val name :String = "Kotlin"
+//Char represents a single 16-bit Unicode character
+// and is defined using single quotes.
+// String represents an immutable sequence of characters
+// and is defined using double quotes
+
+//    String is strictly an Object in Kotlin (and Java).
+//    However, Byte, Int, Double, Boolean, and Char can be optimized
+//    by the compiler to act as primitives in the bytecode for performance,
+//    but String is always an Object
 
 }
