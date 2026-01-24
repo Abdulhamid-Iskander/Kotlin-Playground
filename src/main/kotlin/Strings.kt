c5858463fe57e3158dc3315993e2b703c5153e6d
Abdulhamid-Iskander <abdulhamidiskander@gmail.com>
2026-01-24 08:32:27 +0200

    Update Strings.kt

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -19,4 +19,17 @@ fun main(){
 // The $ symbol is used for variable injection,
 // and ${} is used for evaluating arbitrary expressions directly within the string literal.
 // This is compiled into a highly optimized StringBuilder append sequence
+
+
+val s1 = "Hello"
+val s2 = "Hello"
+val s3 = String("Hello".toCharArray()) // new object
+
+println(s1 == s2)   // true
+println(s1 === s3)  // false
+
+//In Kotlin, the == operator checks for Structural Equality (calls .equals()),
+// meaning it compares the content.
+//The === operator checks for Referential Equality,
+meaning it checks if both variables point to the exact same object in memory.
 }
