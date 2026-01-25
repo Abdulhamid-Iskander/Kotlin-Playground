bbca893a3ce320e4a956bd43c552860ed0f03ea0
caesar <abdulhamidiskander@gmail.com>
2026-01-25 11:40:26 +0200

    Srings Update 2

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -10,8 +10,8 @@ fun main(){
     // its character sequence cannot be changed.
     // Methods like uppercase() do not modify the original string; instead,
     // they return a completely new String object
-     var upper = name.uppercase()
-     println(upper)
+    var upper = name.uppercase()
+    println(upper)
     val age = 20
     val msg = "My age is $age, next year I'll be ${age + 1}"
     println(msg)
@@ -21,15 +21,15 @@ fun main(){
 // This is compiled into a highly optimized StringBuilder append sequence
 
 
-val s1 = "Hello"
-val s2 = "Hello"
-val s3 = String("Hello".toCharArray()) // new object
+    val s1 = "Hello"
+    val s2 = "Hello"
+    val s3 = String("Hello".toCharArray()) // new object
 
-println(s1 == s2)   // true
-println(s1 === s3)  // false
+    println(s1 == s2)   // true
+    println(s1 === s3)  // false
 
 //In Kotlin, the == operator checks for Structural Equality (calls .equals()),
 // meaning it compares the content.
 //The === operator checks for Referential Equality,
-meaning it checks if both variables point to the exact same object in memory.
+//    meaning it checks if both variables point to the exact same object in memory.
 }
