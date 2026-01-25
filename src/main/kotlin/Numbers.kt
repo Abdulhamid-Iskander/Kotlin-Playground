50d83424dae8f4dd28d5ec1554fec4104ab69202
caesar <abdulhamidiskander@gmail.com>
2026-01-25 12:14:35 +0200

    add Numbers.kt

---
+++ src/main/kotlin/Numbers.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // 1. Integers (Whole numbers)
+    val tiny: Byte = 100         // 8-bit
+    val small: Short = 10000     // 16-bit
+    val standard: Int = 100000   // 32-bit (Default)
+    val huge: Long = 10000000L   // 64-bit (Must use 'L')
+
+    // 2. Floating Points (Decimal numbers)
+    val precise: Double = 3.14   // 64-bit (Default & Recommended)
+    val fast: Float = 3.14f      // 32-bit (Must use 'f' or 'F')
+}
