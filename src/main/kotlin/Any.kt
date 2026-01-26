498ba4724e910075bb27587849ee1189c16f60a4
caesar <abdulhamidiskander@gmail.com>
2026-01-26 13:35:59 +0200

    update Any.kt
    Signed-off-by: caesar <abdulhamidiskander@gmail.com>

---
+++ src/main/kotlin/Any.kt
@@ -0,0 +1,8 @@
+fun printAnything(obj: Any) {
+    println(obj)
+} // function accept any things
+fun main (){
+    val name: Any = "Ahmed"   // Works
+    val age: Any = 25         // Works
+    val isDev: Any = true     // Works
+}
