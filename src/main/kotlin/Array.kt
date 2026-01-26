a44a83f796e314785ed3169d04fd365f11d63a52
caesar <abdulhamidiskander@gmail.com>
2026-01-26 12:52:03 +0200

    Update Array.kt
    Signed-off-by: caesar <abdulhamidiskander@gmail.com>

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -12,4 +12,15 @@ Array<T> represents an array of objects (boxed values),
 which incurs memory overhead. Specialized arrays compile down to JVM primitive arrays (e.g., int[]),
 providing contiguous memory allocation and better performance
 */
+//    Fixed Size
+val arr =arrayOfNulls<String>(5)
+    arr[0]="user1"
+    arr[1]="user2"
+    arr[2]="user3"
+    arr[3]="user4"
+    arr[4]="user5"
+//  arr[5]="user6"      Error: IndexOutOfBoundsException
+
+//    Array Constructor
+    val evenNumber =Array(5){i ->(i*2).toString()}
 }
