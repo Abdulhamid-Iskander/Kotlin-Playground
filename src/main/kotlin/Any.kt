76f8374b1b16c186a2c059f209bc8afae94dc87a
caesar <abdulhamidiskander@gmail.com>
2026-01-26 13:38:04 +0200

    update Any.kt 2
    Signed-off-by: caesar <abdulhamidiskander@gmail.com>

--- src/main/kotlin/Any.kt
+++ src/main/kotlin/Any.kt
@@ -5,4 +5,17 @@ fun main (){
     val name: Any = "Ahmed"   // Works
     val age: Any = 25         // Works
     val isDev: Any = true     // Works
+
+    /*Any is the root of the Kotlin class hierarchy.
+     Every class in Kotlin has Any as a superclass,
+     either implicitly or explicitly.
+    It corresponds to java.lang.Object in Java, but with fewer methods*/
+
+    val obj: Any = "Test"
+    obj.toString()
+    obj.hashCode()
+    obj.equals("A")
+
+    val x: Any = "Hello"
+    val y: Any? = null   // Correct
 }
