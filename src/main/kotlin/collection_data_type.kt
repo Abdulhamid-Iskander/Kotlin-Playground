99ca258ed8db530174d3a90f0d865397e58df5b0
Abdulhamid-Iskander <abdulhamidiskander@gmail.com>
2026-02-21 12:34:18 +0200

    Update collection_data_type.kt

--- src/main/kotlin/collection_data_type.kt
+++ src/main/kotlin/collection_data_type.kt
@@ -7,4 +7,11 @@ fun main() {
     // Backed by dynamic data structures.
     val dynamic = mutableListOf("A", "B")
     dynamic.add("C")
+
+    // Set automatically hashes elements and discards duplicates.
+    val set = setOf(1, 2, 2, 3) 
+    println(set) // Output: [1, 2, 3]
+
+    val editable = mutableSetOf("A", "B")
+    editable.add("A") // Ignored, "A" already exists.
 }
